a1b95ed0cd88f181c8b16c45897d6e5cdaddef49
Yigit Akbas <yigitcem.akbas@gmil.com>
2026-06-18 15:25:03 +0300

    Cleaned up repo.

--- main.c
+++
@@ -1,8 +0,0 @@
-// custom ebpf user plane function (UPF) forwarder  
-#include <linux/bpf.h>
-#include <bpf/bpf_helpers.h>
-// Define the license (required by the kernel)
-SEC("license") const char LICENSE[] = "GPL";
-
-// Attach this function to the sys_enter_execve tracepoint
-SEC("tracepoint/syscalls/sys_enter_execve")
